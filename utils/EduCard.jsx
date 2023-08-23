b144bd408a02d8817e866e1e5634760018b46b49
Arth Vhanesa <77712031+ArthVhanesa@users.noreply.github.com>
2023-08-23 10:24:34 +0530

    image change

--- utils/EduCard.jsx
+++ utils/EduCard.jsx
@@ -11,14 +11,13 @@ const EduCard = ({
   return (
     <div className="md:flex items-center gap-3 p-4 bg-sh-dark-500 border-2 border-sh-dark rounded-lg text-sh-white hover:shadow-lg transition-transform transform ease-in hover:scale-[1.02]">
       <div className="flex items-center gap-3">
-        <div className="flex items-center w-14 h-14 md:w-16 md:h-16 rounded-lg overflow-hidden">
+        <div className="flex items-center rounded-lg overflow-hidden">
           <Image
             src={logo}
             alt={institution + " logo"}
-            objectFit="contain"
             draggable="false"
-            width={160}
-            height={160}
+            width={70}
+            height={70}
           />
         </div>
         <h3 className="md:hidden block text-lg md:text-xl font-bold">
