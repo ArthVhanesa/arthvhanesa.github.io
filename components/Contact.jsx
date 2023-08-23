83c77cc0b6644538401eca9fc6ee55de91f36f75
Arth Vhanesa <77712031+ArthVhanesa@users.noreply.github.com>
2023-08-23 14:13:25 +0530

    contact form update

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,15 +5,18 @@ import { Heading } from "../utils";
 export default function Contact() {
   return (
     <>
-      <div className="flex flex-col p-5 justify-evenly items-center m-auto md:px-[40px] px-4 min-h-[300px] bg-sh-dark-500 border-2 border-sh-dark rounded-lg">
-        <h1 className="text-3xl font-bold text-center">Let's Get in Touch</h1>
-        <h1 className="text-xl text-sh-white-500 text-center">
-          I'd love to hear from you! Whether you have a project in mind, want to
-          collaborate, or just want to say hi, feel free to drop me a line.
+      <div className="flex flex-col p-5 items-center m-auto md:px-[40px] px-4 bg-sh-dark-500 border-2 border-sh-dark rounded-lg">
+        <h1 className="text-3xl mb-5 font-bold text-center">
+          Let&apos;s Get in Touch
+        </h1>
+        <h1 className="text-xl mb-5 text-sh-white-500 text-center">
+          I&apos;d love to hear from you! Whether you have a project in mind,
+          want to collaborate, or just want to say hi, feel free to drop me a
+          line.
           <br />
-          I am also open to job opportunities, so if you're looking for someone
-          with my skills, please get in touch. I'll do my best to get back to
-          you as soon as possible.
+          I am also open to job opportunities, so if you&apos;re looking for
+          someone with my skills, please get in touch. I&apos;ll do my best to
+          get back to you as soon as possible.
           <br /> Looking forward to connecting with you!
         </h1>
         <a href="https://tally.so#tally-open=wveqKA&tally-layout=modal&tally-overlay=1&tally-emoji-text=👋&tally-emoji-animation=wave&tally-auto-close=2000">
